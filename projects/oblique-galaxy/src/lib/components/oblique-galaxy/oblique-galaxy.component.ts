770bdc3fb356bca25554999385db8d7f872c7e69
Etienne FRANK <etienne.frank@bit.admin.ch>
2024-03-27 14:30:33 +0100

    Add repository lint first draft

---
+++ projects/oblique-galaxy/src/lib/components/oblique-galaxy/oblique-galaxy.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-oblique-galaxy',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      oblique-galaxy works!
+    </p>
+  `,
+  styles: ``
+})
+export class ObliqueGalaxyComponent {
+
+}
