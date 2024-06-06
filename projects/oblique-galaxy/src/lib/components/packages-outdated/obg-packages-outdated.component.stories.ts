c2b0d60312a3dc7bc64b63202285c0012df1322e
Paulo Lopes <paulo.lopes@bit.admin.ch>
2024-06-06 09:31:05 +0200

    add mocks

--- projects/oblique-galaxy/src/lib/components/packages-outdated/obg-packages-outdated.component.stories.ts
+++ projects/oblique-galaxy/src/lib/components/packages-outdated/obg-packages-outdated.component.stories.ts
@@ -4,7 +4,48 @@ import {HttpClientModule} from '@angular/common/http';
 import {PackagesOutdatedService} from './packages-outdated.service';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
-import {ObliqueModule} from '@oblique/oblique'; // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+import {ObliqueModule} from '@oblique/oblique';
+import {of} from 'rxjs';
+import {Packages} from './packages.model'; // eslint-disable-next-line @typescript-eslint/no-magic-numbers,@typescript-eslint/no-unsafe-assignment
+
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers,@typescript-eslint/no-unsafe-assignment
+const mockPackages: Packages[] = [
+	{
+		name: 'mock-@angular-devkit/build-angular',
+		current: '17.0.3',
+		wanted: '17.0.3',
+		latest: '18.0.0-beta.2',
+		versionDiff: '0'
+	},
+	{
+		name: 'mock-eslint',
+		current: '8.57.0',
+		wanted: '8.57.0',
+		latest: '9.4.0',
+		versionDiff: '0'
+	},
+	{
+		name: 'mock-ng-packagr',
+		current: '17.3.0',
+		wanted: '17.3.0',
+		latest: '18.0.0',
+		versionDiff: '0'
+	},
+	{
+		name: 'mock-prettier',
+		current: '4.3.2',
+		wanted: '4.3.2',
+		latest: '4.3.2',
+		versionDiff: '0'
+	},
+	{
+		name: 'mock-tslib',
+		current: '2.6.2',
+		wanted: '2.6.3',
+		latest: '2.6.3',
+		versionDiff: '0'
+	}
+];
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<ObgPackagesOutdatedComponent> = {
@@ -14,7 +55,15 @@ const meta: Meta<ObgPackagesOutdatedComponent> = {
 	decorators: [
 		moduleMetadata({
 			imports: [HttpClientModule, ObgPackagesOutdatedComponent, BrowserAnimationsModule, MatButtonModule, ObliqueModule],
-			providers: [PackagesOutdatedService]
+			providers: [
+				{
+					provide: PackagesOutdatedService,
+					useValue: {
+						getOutdatedPackages: () => of(mockPackages),
+						getLocalPackages: () => of(mockPackages) // Mock getLocalPackages method
+					}
+				}
+			]
 		})
 	]
 } as Meta;
