84d78bdcdab199116e1a07650243555d1bec69ce
paulo lopes <paulo.lopes@bit.admin.ch>
2024-07-02 11:07:11 +0200

    fix jest tests add detectOpenHandles option

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -5,17 +5,10 @@ globalThis.ngJest = {
 module.exports = {
 	preset: "jest-preset-angular",
 	setupFilesAfterEnv: ["<rootDir>/tests/setupJest.ts"],
+	testTimeout: 1000,
+	verbose: true,
+	detectOpenHandles: true,
 	collectCoverage: true,
-
-	// Ignorer les fichiers de test qui se terminent par .stories.ts
-	testMatch: [
-		"**/*.spec.ts",
-		"!**/*.stories.ts", // Ignorer les fichiers de test se terminant par .stories.ts
-	],
-
-	// Exclure les fichiers de test .stories.ts de la couverture
-	collectCoverageFrom: [
-		"**/*.ts",
-		"!**/*.stories.ts", // Exclure les fichiers .stories.ts de la couverture
-	],
+	testMatch: ["**/*.spec.ts", "!**/*.stories.ts"],
+	collectCoverageFrom: ["**/*.ts", "!**/*.stories.ts"],
 };
