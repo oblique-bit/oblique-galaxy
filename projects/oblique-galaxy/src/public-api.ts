2150495eebb48d2d52d690d85d44a465aab6062d
paulo lopes <paulo.lopes@bit.admin.ch>
2024-10-06 11:57:09 +0200

    feat(snackBar): add snackbar on public-api.ts

--- projects/oblique-galaxy/src/public-api.ts
+++ projects/oblique-galaxy/src/public-api.ts
@@ -7,3 +7,4 @@ export * from './lib/components/textarea-input/textarea-input.component';
 export * from './lib/components/convert-json-to-excel/convert-json-to-excel.component';
 export * from './lib/components/convert-excel-to-json/convert-excel-to-json.component';
 export * from './lib/components/command/command.component';
+export * from './lib/components/snack-bar/obg-snack-bar.component';
