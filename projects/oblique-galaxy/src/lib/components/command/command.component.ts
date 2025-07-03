966bc88a1e4945cc082ef2eff0fede3f82af8daf
Pantazis Marina BIT <marina.pantazis@bit.admin.ch>
2025-07-03 11:51:48 +0200

    fix: obIcon display again and css import warning fixed and fixed vulnerabilities

--- projects/oblique-galaxy/src/lib/components/command/command.component.ts
+++ projects/oblique-galaxy/src/lib/components/command/command.component.ts
@@ -2,13 +2,10 @@ import {AfterViewInit, Component, ElementRef, signal, viewChild} from '@angular/
 import {CdkCopyToClipboard} from '@angular/cdk/clipboard';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltip} from '@angular/material/tooltip';
-import {ObIconModule} from '@oblique/oblique';
-
 @Component({
 	selector: 'obg-command',
 	standalone: true,
-	imports: [ObIconModule, CdkCopyToClipboard, MatTooltip, MatIconModule],
-	providers: [],
+	imports: [CdkCopyToClipboard, MatTooltip, MatIconModule],
 	templateUrl: './command.component.html',
 	styleUrl: './command.component.scss'
 })
